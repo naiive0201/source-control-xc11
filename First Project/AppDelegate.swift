3b213208906a72acd72e6fbbba662f88c4ae2379
hyunsooju <hyunsooting@gmail.com>
2020-10-12 22:52:57 +0900

    Initial Commit

---
+++ First Project/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  First Project
+//
+//  Created by hyunsoo Ju on 2020/10/12.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ First Project/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  First Project
+//
+//  Created by hyunsoo Ju on 2020/10/12.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+    
+    /**
+     Adds two numbers together and returns the result.
+     - parameter num1: The first number
+     - parameter num2:  The second number
+     - returns: The sum of num1 and num2
+     */
+    func addNumbers(num1: Int, num2: Int) -> Int {
+        return num1 + num2;
+    }
+    
+    
+
+
+}
+
